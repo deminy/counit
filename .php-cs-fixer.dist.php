33ee04275c5ea6c14aca08b4283c7496ac8f575c
Demin Yin <deminy@deminy.net>
2021-09-14 12:47:14 -0700

    updates for upgrading PHP CS Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@DoctrineAnnotation'                    => true,
@@ -10,7 +10,7 @@ return PhpCsFixer\Config::create()
         '@PSR2'                                  => true,
         '@Symfony'                               => true,
         'array_syntax'                           => ['syntax' => 'short'],
-        'binary_operator_spaces'                 => ['align_double_arrow' => true, 'align_equals' => true],
+        'binary_operator_spaces'                 => ['operators' => ['=' => 'align', '=>' => 'align', ]],
         'blank_line_after_namespace'             => true,
         'blank_line_before_statement'            => ['statements' => ['declare']],
         'class_attributes_separation'            => true,
@@ -19,7 +19,7 @@ return PhpCsFixer\Config::create()
         'combine_consecutive_unsets'             => true,
         'declare_strict_types'                   => true,
         'general_phpdoc_annotation_remove'       => ['annotations' => ['author']],
-        'header_comment'                         => ['commentType' => 'PHPDoc', 'header' => '', 'separate' => 'bottom', 'location' => 'after_open'],
+        'header_comment'                         => ['comment_type' => 'PHPDoc', 'header' => '', 'separate' => 'bottom', 'location' => 'after_open'],
         'increment_style'                        => ['style' => 'post'],
         'lambda_not_used_import'                 => false,
         'linebreak_after_opening_tag'            => true,
